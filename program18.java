d0805ab62da7f2ba47f0a3d0eda0fa3c7d75fea9
jmore8300 <jmore8300@gmail.com>
2025-11-21 23:53:20 +0530

    Add files via upload

---
+++ program18.java
@@ -0,0 +1,17 @@
+// worst approach
+
+
+class program18
+{
+   public static void main(String A[])
+   {
+
+    //  Sequance
+
+       System.out.println("Jay Ganesh...");
+       System.out.println("Jay Ganesh...");
+       System.out.println("Jay Ganesh...");
+       System.out.println("Jay Ganesh...");
+   }
+
+}
